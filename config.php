34da605111d6f59f81d3576cbff15025db14782c
aveekdeveloper <aveekdeveloper@gmail.com>
2015-03-01 23:37:35 +0530

    Initial version
    A working version with only listings

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * The base MySQL settings of Osclass
+ */
+define('MULTISITE', 0);
+
+/** MySQL database name for Osclass */
+define('DB_NAME', 'osclass');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'password');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Table prefix */
+define('DB_TABLE_PREFIX', 'oc_');
+
+define('REL_WEB_URL', '/mysite/');
+
+define('WEB_PATH', 'http://127.0.0.1/mysite/');
+
+//define('OSC_DEBUG_DB' , true)
+
+?>
---
+++ oc-content/themes/bender/item-sidebar.php
@@ -0,0 +1,33 @@
+<?php
+    /*
+     *      Osclass – software for creating and publishing online classified
+     *                           advertising platforms
+     *
+     *                        Copyright (C) 2014 OSCLASS
+     *
+     *       This program is free software: you can redistribute it and/or
+     *     modify it under the terms of the GNU Affero General Public License
+     *     as published by the Free Software Foundation, either version 3 of
+     *            the License, or (at your option) any later version.
+     *
+     *     This program is distributed in the hope that it will be useful, but
+     *         WITHOUT ANY WARRANTY; without even the implied warranty of
+     *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     *             GNU Affero General Public License for more details.
+     *
+     *      You should have received a copy of the GNU Affero General Public
+     * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+     */
+?>
+<div id="sidebar">
+    
+    <?php if( osc_get_preference('sidebar-300x250', 'bender') != '') {?>
+    <!-- sidebar ad 350x250 -->
+    <div class="ads_300">
+        <?php echo osc_get_preference('sidebar-300x250', 'bender'); ?>
+    </div>
+    <!-- /sidebar ad 350x250 -->
+    <?php } ?>
+
+    
+</div><!-- /sidebar -->
