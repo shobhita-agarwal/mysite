b94fa1a99ffb5157439c8b36a058407f971f666d
aveekdeveloper <aveekdeveloper@gmail.com>
2015-03-25 20:23:06 +0530

    User dashboard show reviews

--- oc-content/themes/realestate/user-dashboard.php
+++ oc-content/themes/realestate/user-dashboard.php
@@ -22,59 +22,17 @@
 <?php osc_current_web_theme_path('header.php') ; ?>
 <div class="content user-area">
     <div id="right-side">
-        <h1><?php _e('User account manager', 'realestate') ; ?></h1>
-        <h2><?php echo "Your Listings"; echo "(".osc_count_items().")";?></h2>
-        <div class="ad_list">
-            <?php if(osc_count_items() == 0) { ?>
-            <h3><?php _e('No listings have been added yet', 'realestate'); ?></h3>
-        <?php } else { ?>
-            <?php while(osc_has_items()) { ?>
-            <div class="ui-item ui-item-list">
-                <div class="frame">
-                    <a href="<?php echo osc_item_url() ; ?>"><?php if( osc_images_enabled_at_items() ) { ?>
-                        <?php if( osc_count_item_resources() ) { ?>
-                            <img src="<?php echo osc_resource_thumbnail_url() ; ?>" title="<?php echo osc_item_title(); ?>" alt="<?php echo osc_item_title(); ?>"/>
-                        <?php } else { ?>
-                            <img src="<?php echo osc_current_web_theme_url('images/no_photo.gif') ; ?>" alt="" title=""/>
-                        <?php } ?>
-                    <?php } else { ?>
-                        <img src="<?php echo osc_current_web_theme_url('images/no_photo.gif') ; ?>" alt="" title=""/>
-                    <?php } ?>
-                    <div class="type"><?php echo osc_item_category(); ?></div>
-                    <?php if( osc_price_enabled_at_items() ) { ?><div class="price"><?php echo osc_item_formated_price() ; ?></div> <?php } ?>
-                    </a>
-                </div>
-                <div class="info">
-                    <div>
-                        <h3><a href="<?php echo osc_item_url() ; ?>"><?php if(strlen(osc_item_title()) > 31){ echo substr(osc_item_title(), 0, 28).'...'; } else { echo osc_item_title(); } ?></a></h3>
-                    </div>
-                    <div class="data data-full">
-                        <?php _e('Publication date', 'realestate') ; ?>: <?php echo osc_format_date(osc_item_pub_date()) ; ?><br />
-                        <div>
-                        <a href="<?php echo osc_item_url(); ?>" class="ui-button ui-button-grey ui-button-mini"><?php _e('View item', 'realestate'); ?></a>
-                        <a href="<?php echo osc_item_edit_url(); ?>" class="ui-button ui-button-grey ui-button-mini"><?php _e('Edit', 'realestate'); ?></a>
-                        <?php if(osc_item_is_inactive()) {?>
-                        <a href="<?php echo osc_item_activate_url();?>" class="ui-button ui-button-grey ui-button-mini"><?php _e('Activate', 'realestate'); ?></a>
-                        <?php } ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <?php } ?>
-        <?php } ?>
-        </div>
-        
+        <h1><?php _e('Dashboard', 'realestate') ; ?></h1>
+               
 
 		<?php/* comment section*/?>
-		<div class="main-review">
-                    <div id="description">
-                        
-                    
+		<div class="content-item ">
+                    <div id="description">                    
                     <!-- plugins -->
                     <?php if( osc_comments_enabled() ) { ?>
                         <?php if( osc_reg_user_post_comments () && osc_is_web_user_logged_in() || !osc_reg_user_post_comments() ) { ?>
                         <div id="comments">
-                            <h2><?php _e('Your Reviews', 'realestate'); echo "(".osc_count_user_comments().")";?></h2>
+                            <h2><?php _e('Your Reviewed venues', 'realestate'); echo "(".osc_count_user_comments().")";?></h2>
 							<?php if( osc_count_user_comments() ==0 ) { ?>
 							<h3><?php _e('No Reviews have been posted yet', 'realestate'); ?></h3>
 							<?php }?>
@@ -84,13 +42,16 @@
                                     <ul class="reviews-list">
                                     <?php while ( osc_has_user_comments() ) { ?>
                                         <li class="reviews-list-item">
-                                          
-                                          <div class="comments">
+                                          <div class="profile_pic">
+                                              <img alt="James" src="<?php echo get_profile_image(osc_comment_author_email()); ?>" title="<?php echo osc_comment_author_name() ; ?>">
+                                              <?php echo osc_comment_author_name() ; ?>
+                                          </div>
+                                          <div class="message">
 										    <h2 class="item-heading">
 											<?php 
 											$item = (Item::newInstance()->findByPrimaryKey(osc_comment_item_id())); 
 											//print_r ($item);
-											$item_title = $item['s_title']." " .$item['s_city'];
+											$item_title = $item['s_title'].", " .$item['s_city'];
 											?>
 											<a href="<?php echo osc_item_url_ns(osc_comment_item_id()) ; ?>"><?php if(strlen($item_title) > 31){ 
 											echo substr($item_title, 0, 28).'...'; } else { echo $item_title; } ?></a></h2>
@@ -122,59 +83,6 @@
         </div>
 		<?php /* end comment section*/?>	
 		
-		<?php /* start alert section*/?>
-		<div class="main-alert">
-		<h2><?php _e('Your Alerts', 'realestate'); if(osc_count_alerts()>1) {echo "(".osc_count_alerts().")";} else{echo "(0)";}?> </h2>
-            <?php if(osc_count_alerts()< 1) { ?>
-                    <h3><?php _e('You do not have any alerts yet', 'realestate'); ?>.</h3>
-                <?php } else { ?>
-                    <?php while(osc_has_alerts()) { ?>
-                        <div class="user-alert-title" >
-                            <?php _e('Alert', 'realestate'); ?> | 
-							<a onclick="javascript:return confirm('
-                            <?php _e('This action can\'t be undone.   Are you sure you want to continue?', 'realestate'); ?>');" 
-							class="ui-button ui-button-red ui-button-mini" href="<?php echo osc_user_unsubscribe_alert_url() ; ?>
-							"><?php _e('Delete this alert', 'realestate') ; ?>
-							</a>
-                        </div>
-                        <?php while(osc_has_items()) { ?>
-                            <div class="ui-item ui-item-list">
-                                <div class="frame">
-                                    <a href="<?php echo osc_item_url() ; ?>"><?php if( osc_images_enabled_at_items() ) { ?>
-                                        <?php if( osc_count_item_resources() ) { ?>
-                                            <img src="<?php echo osc_resource_thumbnail_url() ; ?>" title=
-											"<?php echo osc_item_title(); ?>" alt=" <?php echo osc_item_title(); ?>"/>
-                                             <?php } else { ?>
-                                            <img src="<?php echo osc_current_web_theme_url('images/no_photo.gif') ; ?>" alt="" title=""/>
-                                        <?php } ?>
-                                    <?php } else { ?>
-                                        <img src="<?php echo osc_current_web_theme_url('images/no_photo.gif') ; ?>" alt="" title=""/>
-                                    <?php } ?>
-                                    <div class="type"><?php echo osc_item_category(); ?></div>
-                                    <?php if( osc_price_enabled_at_items() ) { ?><div class="price"><?php echo osc_item_formated_price() ; ?></div>                                    <?php } ?>
-                                    </a>
-                                </div>
-                                <div class="info">
-                                    <div>
-                                        <h3><a href="<?php echo osc_item_url() ; ?>"><?php if(strlen(osc_item_title()) > 31){ 
-										echo substr(osc_item_title(), 0, 28).'...'; } else { echo osc_item_title(); } ?></a></h3>
-                                    </div>
-                                    <div class="data data-full">
-                                        <?php _e('Publication date', 'realestate') ; ?>: <?php echo osc_format_date(osc_item_pub_date()) ; ?><br />
-                                        <div>
-                                        <a href="<?php echo osc_item_url(); ?>" 
-										class="ui-button ui-button-grey ui-button-mini"><?php _e('View item', 'realestate'); ?></a>
-                                        <a href="<?php echo osc_item_url(); ?>#contact" 
-										class="ui-button ui-button-grey ui-button-mini"><?php _e('Contact the publisher', 'realestate'); ?></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <?php } ?>
-                    <?php } ?>
-                <?php  } ?>
-        </div>
-		<?php /* end alert section*/?>
     </div>
     <?php require('user_sidebar.php') ; ?>
     <div class="clear"></div>
